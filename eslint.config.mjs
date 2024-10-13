13c20cd89c9239a61d3cc833cb5588628a3be71d
Shahzod <shahzodwill@gmail.com>
2024-10-13 17:59:25 +0200

    add jest plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,44 +1,51 @@
-import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [...compat.extends(
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:prettier/recommended",
-), {
+export default [
+  ...compat.extends(
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:jest/recommended'
+  ),
+  {
     languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...globals.node,
-        },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
+      },
 
-        ecmaVersion: 2020,
-        sourceType: "module",
+      ecmaVersion: 2020,
+      sourceType: 'module',
     },
 
     settings: {
-        react: {
-            version: "detect",
-        },
+      react: {
+        version: 'detect',
+      },
     },
 
+    plugins: ['jest'],
+
     rules: {
-        semi: ["error", "always"],
-        quotes: ["error", "single"],
-        indent: ["error", 2],
-        "no-unused-vars": "warn",
-        "react/react-in-jsx-scope": "off",
-        "prettier/prettier": "error",
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      indent: ['error', 2],
+      'no-unused-vars': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'prettier/prettier': 'error',
     },
-}];
+  },
+];
